59863b4483cd0cd586f9efd854678559e4396f4c
howa8068 <hong.wang.8068@student.uu.se>
2024-03-18 21:15:43 +0100

    speech added

--- script.js
+++ script.js
@@ -82,6 +82,121 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     });
 
+    document.getElementById('b3').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=maybe you could integrate the information you have received by discussion';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+    document.getElementById('b4').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=Please can we stay focus on the task';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+    document.getElementById('b5').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=The discussion time has passed halfway, with fifteen minutes remaining';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+    document.getElementById('b6').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=Sorry to interrupt you, now the discussion session ended';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+    document.getElementById('b7').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=Let us go ahead and present the plan you have settled on';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+    document.getElementById('b8').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=Sounds like a perfect solution, you guys are amazing.  Thank you for your participation, see you next time';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+
     document.getElementById('b9').addEventListener('click', function () {
         const url = 'http://192.168.137.1:54321/furhat/say?Text=It might be a good idea to take a look at the hints on the collaboration script';
         fetch(url, {
@@ -120,7 +235,165 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     });
 
+    document.getElementById('b11').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=What is your opinion about this';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
 
+    document.getElementById('b12').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=Remember that we are a team, we need to cooperate';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+
+    //turn left and right
+
+    document.getElementById('b15').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=I agree';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+    document.getElementById('b16').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=I do not agree with this';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+    document.getElementById('b17').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=Great job everyone';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+    document.getElementById('b18').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=Could you explain more what you mean';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+
+
+    //nod
+    document.getElementById('b19').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=It might be a good idea to take a look at the hints on the collaboration script';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+
+
+    //smile
+    document.getElementById('b20').addEventListener('click', function () {
+        const url = 'http://192.168.137.1:54321/furhat/say?Text=Do not forget that we have scripts to reference';
+        fetch(url, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Furhat said:', data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
 
 
 });
